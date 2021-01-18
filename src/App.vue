71eafe7c4c084ce821c88e9b6dcdbb1195727e78
Guilherme Samuel <guisamuel800@gmail.com>
2021-01-18 20:46:14 -0300

    Task list (part 1)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TaskGrid :tasks="tasks">
 	</div>
 </template>
 
 <script>
+
+import TaskGrid from './components/TaskGrid.vue'
+
 export default {
+	components: {TaskGrid},
 	data() {
 		return {
 			tasks: [
